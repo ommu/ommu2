ddbe0306b71d44cd597c717767d557dd16dc6a30
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-12-22 03:30:35 +0700

    add admin module source-code

---
+++ protected/modules/admin/Module.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * admin module definition class
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 OMMU (www.ommu.co)
+ * @created date 22 December 2018, 03:29 WIB
+ * @link https://github.com/ommu/ommu
+ *
+ */
+
+namespace app\modules\admin;
+
+class Module extends \app\components\Module
+{
+	public $layout = 'main';
+
+	/**
+	 * @inheritdoc
+	 */
+	public $controllerNamespace = 'app\modules\admin\controllers';
+
+	/**
+	 * @inheritdoc
+	 */
+	public function init()
+	{
+		parent::init();
+
+		// custom initialization code goes here
+	}
+}
---
+++ protected/modules/admin/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * admin module config
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 OMMU (www.ommu.co)
+ * @created date 22 December 2018, 03:29 WIB
+ * @link https://github.com/ommu/ommu
+ *
+ */
+
+return [
+	'id' => 'admin',
+	'class' => app\modules\admin\Module::className(),
+];
