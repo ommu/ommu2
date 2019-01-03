60ccd26e867ba6a9d6c109d9bea5e4a2693301a0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-01-03 10:39:01 +0700

    add dashboard

---
+++ protected/modules/admin/controllers/DashboardController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * DashboardController
+ * @var $this yii\web\View
+ *
+ * Reference start
+ * TOC :
+ *	Index
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 OMMU (www.ommu.co)
+ * @created date 3 January 2018, 00:24 WIB
+ * @link https://github.com/ommu/ommu
+ *
+ */
+
+namespace app\modules\admin\controllers;
+
+use app\components\Controller;
+use mdm\admin\components\AccessControl;
+
+class DashboardController extends Controller
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function behaviors()
+	{
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+			],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function allowAction(): array {
+		return [];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function actionIndex()
+	{
+		$this->view->title = 'DashboardControllers';
+		$this->view->description = '';
+		$this->view->keywords = '';
+		return $this->render('admin_index');
+	}
+
+}
---
+++ protected/modules/admin/views/dashboard/admin_index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @var $this yii\web\View
+ * @var $this app\modules\admin\controllers\DashboardController
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 OMMU (www.ommu.co)
+ * @created date 3 January 2018, 00:24 WIB
+ * @link https://github.com/ommu/ommu
+ *
+ */
+
+use yii\helpers\Html;
+?>
+
+<p>
+	You may change the content of this page by modifying
+	the file <code><?php echo __FILE__; ?></code>.
+</p>
