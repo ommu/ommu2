3c3f482bf6f4dbb564b03f0d075e9ad8bb016a5b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-01-03 11:01:51 +0700

    commit

--- protected/modules/rbac/components/MenuHelper.php
+++ protected/modules/rbac/components/MenuHelper.php
@@ -17,5 +17,42 @@ use mdm\admin\components\MenuHelper as MdmMenuHelper;
 
 class MenuHelper extends MdmMenuHelper
 {
-	
+	/**
+	 * Normalize menu
+	 * @param  array $assigned
+	 * @param  array $menus
+	 * @param  Closure $callback
+	 * @param  integer $parent
+	 * @return array
+	 */
+	public static function normalizeMenu(&$assigned, &$menus, $callback, $parent = null)
+	{
+		$result = [];
+		$order = [];
+		foreach ($assigned as $id) {
+			$menu = $menus[$id];
+			if ($menu['parent'] == $parent) {
+				$menu['children'] = static::normalizeMenu($assigned, $menus, $callback, $id);
+				if ($callback !== null) {
+					$item = call_user_func($callback, $menu);
+				} else {
+					$item = [
+						'label' => $menu['name'],
+						'url' => static::parseRoute($menu['route']),
+						'icon' => $menu['icon'],
+					];
+					if ($menu['children'] != []) {
+						$item['items'] = $menu['children'];
+					}
+				}
+				$result[] = $item;
+				$order[] = $menu['order'];
+			}
+		}
+		if ($result != []) {
+			array_multisort($order, $result);
+		}
+
+		return $result;
+	}
 }
