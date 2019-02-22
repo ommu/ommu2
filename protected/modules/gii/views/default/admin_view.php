3cf7558b963ea560871caea760a066658bcf8831
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-02-22 15:51:04 +0700

    commit

--- protected/modules/gii/views/default/admin_view.php
+++ protected/modules/gii/views/default/admin_view.php
@@ -30,58 +30,55 @@ foreach ($generator->templates as $name => $path) {
 ?>
 
 <div class="default-view">
-	<div class="x_panel">
-		<div class="x_content">
-			<?php $form = ActiveForm::begin([
-				'id' => "$id-generator",
-				'successCssClass' => '',
-				'fieldConfig' => ['class' => ActiveField::className()],
-			]); ?>
+	<?php $form = ActiveForm::begin([
+		'options' => ['class' => 'form-horizontal form-label-left'],
+		'id' => "$id-generator",
+		'successCssClass' => '',
+		'fieldConfig' => ['class' => ActiveField::className()],
+	]); ?>
 
-			<?php echo $this->renderFile($generator->formView(), [
-				'generator' => $generator,
-				'form' => $form,
-			]); ?>
+	<?php echo $this->renderFile($generator->formView(), [
+		'generator' => $generator,
+		'form' => $form,
+	]); ?>
 
-			<?php echo $form->field($generator, 'template', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12">{input}{error}</div>{hint}'])->sticky()
-				->label('Code Template', ['class'=>'control-label col-md-3 col-sm-3 col-xs-12'])
-				->dropDownList($templates)->hint('Please select which set of the templates should be used to generated the code.') ?>
+	<?php echo $form->field($generator, 'template', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12">{input}{error}</div>{hint}'])->sticky()
+		->label('Code Template', ['class'=>'control-label col-md-3 col-sm-3 col-xs-12'])
+		->dropDownList($templates)->hint('Please select which set of the templates should be used to generated the code.') ?>
 
-			<div class="ln_solid"></div>
-			<div class="form-group">
-				<div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-					<?php echo Html::submitButton('Preview', ['name' => 'preview', 'class' => 'btn btn-primary']) ?>
-					<?php if (isset($files)): ?>
-						<?php echo Html::submitButton('Generate', ['name' => 'generate', 'class' => 'btn btn-success']) ?>
-					<?php endif; ?>
-				</div>
-			</div>
-
-			<?php if (isset($results) || isset($files)) {?>
-			<div class="ln_solid"></div>
-			<div class="default-view">
-				<div class="x_panel">
-					<div class="x_content">
-						<?php if (isset($results)) {
-							echo $this->render('@yii/gii/views/default/view/results', [
-								'generator' => $generator,
-								'results' => $results,
-								'hasError' => $hasError,
-							]);
-						} elseif (isset($files)) {
-							echo $this->render('@yii/gii/views/default/view/files', [
-								'id' => $id,
-								'generator' => $generator,
-								'files' => $files,
-								'answers' => $answers,
-							]);
-						}?>
-					</div>
-				</div>
-			</div>
-			<?php }?>
-
-			<?php ActiveForm::end(); ?>
+	<div class="ln_solid"></div>
+	<div class="form-group row">
+		<div class="col-md-6 col-sm-9 col-xs-12 offset-sm-3">
+			<?php echo Html::submitButton('Preview', ['name' => 'preview', 'class' => 'btn btn-primary']) ?>
+			<?php if (isset($files)): ?>
+				<?php echo Html::submitButton('Generate', ['name' => 'generate', 'class' => 'btn btn-success']) ?>
+			<?php endif; ?>
 		</div>
 	</div>
+
+	<?php if (isset($results) || isset($files)) {?>
+	<div class="ln_solid"></div>
+	<div class="default-view">
+		<div class="x_panel">
+			<div class="x_content">
+				<?php if (isset($results)) {
+					echo $this->render('@yii/gii/views/default/view/results', [
+						'generator' => $generator,
+						'results' => $results,
+						'hasError' => $hasError,
+					]);
+				} elseif (isset($files)) {
+					echo $this->render('@yii/gii/views/default/view/files', [
+						'id' => $id,
+						'generator' => $generator,
+						'files' => $files,
+						'answers' => $answers,
+					]);
+				}?>
+			</div>
+		</div>
+	</div>
+	<?php }?>
+
+	<?php ActiveForm::end(); ?>
 </div>
