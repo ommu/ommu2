a6423b6d4afb776e83ceca23cc36edeb0595cff1
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-02-28 10:09:32 +0700

    modify form component

--- protected/components/ActiveForm.php
+++ protected/components/ActiveForm.php
@@ -1,31 +1,28 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
-
-namespace app\components;
-
-use Yii;
-use yii\helpers\ArrayHelper;
-
-/**
  * ActiveForm is a widget that builds an interactive HTML form for one or multiple data models.
  *
  * For more details and usage information on ActiveForm, see the [guide article on forms](guide:input-forms).
  * @see yii\widgets\ActiveForm
+ * @see yii\bootstrap\ActiveForm
  * 
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
  * @created date 11 January 2019, 11:11 WIB
+ * @modified date 27 February 2019, 12:47 WIB
  * @link https://github.com/ommu/ommu
  */
-class ActiveForm extends \yii\widgets\ActiveForm
+
+namespace app\components;
+
+use Yii;
+
+class ActiveForm extends \yii\bootstrap\ActiveForm
 {
 	/**
-	 * {@inheritdoc}
+	 * @var string the default field class name when calling [[field()]] to create a new field.
+	 * @see fieldConfig
 	 */
 	public $fieldClass = 'app\components\widgets\ActiveField';
 
@@ -35,34 +32,12 @@ class ActiveForm extends \yii\widgets\ActiveForm
 	 */
 	public function init()
 	{
+		if(isset($this->options['class'])) {
+			if(preg_match('/(form-horizontal)/', $this->options['class']))
+				$this->layout = 'horizontal';
+			if(preg_match('/(form-inline)/', $this->options['class']))
+				$this->layout = 'inline';
+		}
 		parent::init();
-	}
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function field($model, $attribute, $options=[])
-	{
-		$horizontal = isset($this->options['class']) && preg_match('/(form-horizontal)/', $this->options['class']) ? true : false;
-		if(!array_key_exists('template', $options) && $horizontal)
-			$options = ArrayHelper::merge(['template'=>'{label}<div class="col-md-6 col-sm-9 col-xs-12">{input}{error}{hint}</div>'], $options);
-
-		$config = $this->fieldConfig;
-		if ($config instanceof \Closure) {
-			$config = call_user_func($config, $model, $attribute);
-		}
-		if (!isset($config['class'])) {
-			$config['class'] = $this->fieldClass;
-			if($horizontal) {
-				$config['options'] = ['class'=>'form-group row'];
-				$config['labelOptions'] = ['class'=>'control-label col-md-3 col-sm-3 col-xs-12'];
-			}
-		}
-
-		return Yii::createObject(ArrayHelper::merge($config, $options, [
-			'model' => $model,
-			'attribute' => $attribute,
-			'form' => $this,
-		]));
 	}
 }
--- protected/components/widgets/ActiveField.php
+++ protected/components/widgets/ActiveField.php
@@ -1,19 +1,69 @@
 <?php
 /**
- * ActiveField
+ * ActiveField represents a form input field within an [[ActiveForm]].
+ *
+ * For more details and usage information on ActiveField, see the [guide article on forms](guide:input-forms).
+ * @see yii\widgets\ActiveField
+ * @see yii\bootstrap\ActiveField
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
  * @created date 21 February 2019, 15:29 WIB
+ * @modified date 27 February 2019, 12:47 WIB
  * @link https://github.com/ommu/ommu
  */
 
 namespace app\components\widgets;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
-class ActiveField extends \yii\widgets\ActiveField
+class ActiveField extends \yii\bootstrap\ActiveField
 {
-	
+	/**
+	 * @param array $instanceConfig the configuration passed to this instance's constructor
+	 * @return array the layout specific default configuration for this instance
+	 */
+	protected function createLayoutConfig($instanceConfig)
+	{
+		$layoutConfig  = parent::createLayoutConfig($instanceConfig);
+		$layout = $instanceConfig['form']->layout;
+
+		$layoutConfig = ArrayHelper::merge(
+			$layoutConfig, [
+				'hintOptions' => [
+					'tag' => 'div',
+					'class' => 'hint-block',
+				],
+				'errorOptions' => [
+					'tag' => 'div',
+					'class' => 'help-block',
+				],
+			]);
+
+		if ($layout === 'horizontal') {
+			$config['template'] = "{label}\n{beginWrapper}\n{input}\n{error}\n{hint}\n{endWrapper}";
+			$cssClasses = array_merge([
+				'offset' => 'col-sm-offset-3',
+				'label' => 'col-sm-3',
+				'wrapper' => 'col-sm-6',
+				'error' => '',
+				'hint' => '',
+			], $this->horizontalCssClasses);
+			if (isset($instanceConfig['horizontalCssClasses'])) {
+				$cssClasses = ArrayHelper::merge($cssClasses, $instanceConfig['horizontalCssClasses']);
+			}
+			$config['horizontalCssClasses'] = $cssClasses;
+			$config['options'] = ['class' => 'form-group row'];
+			$config['labelOptions'] = ['class' => 'control-label ' . $cssClasses['label']];
+			$config['wrapperOptions'] = ['class' => $cssClasses['wrapper']];
+			$config['errorOptions']['class'] = 'help-block ' . $cssClasses['error'];
+			$config['hintOptions']['class'] = 'hint-block ' . $cssClasses['hint'];
+			
+			$layoutConfig = ArrayHelper::merge($layoutConfig, $config);
+		}
+
+		return $layoutConfig;
+	}
 }
