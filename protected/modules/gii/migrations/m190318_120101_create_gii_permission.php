65f9cc73faa2dfe83c65c8a68f8bab7a55ba7a47
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-03-18 19:38:48 +0700

    add gii migrations

---
+++ protected/modules/gii/migrations/m190318_120101_create_gii_permission.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * m190318_120101_create_gii_permission
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 18 March 2019, 19:04 WIB
+ * @link https://github.com/ommu/ommu
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m190318_120101_create_gii_permission extends \yii\db\Migration
+{
+	public function up()
+	{
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_auth_item';
+		if(Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert('ommu_core_auth_item', ['name', 'type', 'data', 'created_at'], [
+				['/gii/*', '2', '', time()],
+			]);
+		}
+
+		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_auth_item_child';
+		if(Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert('ommu_core_auth_item_child', ['parent', 'child'], [
+				['userModerator', '/gii/*'],
+			]);
+		}
+	}
+}
