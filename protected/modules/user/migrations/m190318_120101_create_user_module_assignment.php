509e29a155663e42b6b1ff3554aa6f98e6dc6d24
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-03-19 06:21:28 +0700

    commit

--- protected/modules/user/migrations/m190318_120101_create_user_module_assignment.php
+++ protected/modules/user/migrations/m190318_120101_create_user_module_assignment.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * m190318_120101_create_user_module_role
+ * m190318_120101_create_user_module_assignment
  * 
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
@@ -13,7 +13,7 @@
 use Yii;
 use yii\db\Schema;
 
-class m190318_120101_create_user_module_role extends \yii\db\Migration
+class m190318_120101_create_user_module_assignment extends \yii\db\Migration
 {
 	public function up()
 	{
