a7e45d35a6e3a906fd7e6cb31bbc424754147b36
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-08 09:46:40 +0700

    fixed ActiveForm with yii bootstrap(3) and bootstrap4 condition

--- protected/components/widgets/ActiveForm.php
+++ protected/components/widgets/ActiveForm.php
@@ -3,8 +3,8 @@
  * ActiveForm is a widget that builds an interactive HTML form for one or multiple data models.
  *
  * For more details and usage information on ActiveForm, see the [guide article on forms](guide:input-forms).
- * @see yii\widgets\ActiveForm
  * @see yii\bootstrap\ActiveForm
+ * @see yii\widgets\ActiveForm
  * 
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
@@ -17,27 +17,95 @@
 namespace app\components\widgets;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 class ActiveForm extends \yii\bootstrap\ActiveForm
 {
 	/**
-	 * @var string the default field class name when calling [[field()]] to create a new field.
-	 * @see fieldConfig
+	 * {@inheritdoc}
 	 */
-	public $fieldClass = 'app\components\widgets\ActiveField';
+	public static function begin($config = [])
+	{
+		$config = ArrayHelper::merge(
+			$config, 
+			self::getLayout($config)
+		);
+		if(isset(Yii::$app->view->themeSetting['bootstrap4']) && Yii::$app->view->themeSetting['bootstrap4']) {
+			$config = ArrayHelper::merge(
+				$config, 
+				['fieldConfig' => [
+					'labelOptions' => [
+						'class' => ['col-form-label', 'col-md-4 col-sm-3 col-12'],
+					],
+					'wrapperOptions' => [
+						'class' => 'col-md-8 col-sm-9 col-12',
+					],
+				]],
+			);
+		} else {
+			$config = ArrayHelper::merge(
+				$config, 
+				['fieldConfig' => [
+					'options' => [
+						'class' => ['form-group', 'row'],
+					],
+					'labelOptions' => [
+						'class' => ['control-label', 'col-md-3 col-sm-3 col-xs-12'],
+					],
+					'wrapperOptions' => [
+						'class' => 'col-md-6 col-sm-9 col-xs-12',
+					],
+					'hintOptions' => [
+						'tag' => 'div',
+						'class' => 'hint-block',
+					],
+					'errorOptions' => [
+						'tag' => 'div',
+						'class' => ['help-block', 'help-block-error'],
+					],
+				]],
+			);
+			if(self::getLayout($config)['layout'] === 'horizontal') {
+				$config = ArrayHelper::merge(
+					$config, 
+					['fieldConfig' => [
+						'template' => "{label}\n{beginWrapper}\n{input}\n{error}\n{hint}\n{endWrapper}",
+					]],
+				);
+			}
+		}
+
+		$parentClass = get_parent_class();
+		if(isset(Yii::$app->view->themeSetting['bootstrap4']) && Yii::$app->view->themeSetting['bootstrap4'])
+			$parentClass = 'yii\bootstrap4\ActiveForm';
+
+		return  $parentClass::begin($config);
+	}
 
 	/**
-	 * Initializes the widget.
-	 * This renders the form open tag.
+	 * {@inheritdoc}
 	 */
-	public function init()
+	public static function end()
 	{
-		if(isset($this->options['class'])) {
-			if(preg_match('/(form-horizontal)/', $this->options['class']))
-				$this->layout = 'horizontal';
-			if(preg_match('/(form-inline)/', $this->options['class']))
-				$this->layout = 'inline';
-		}
-		parent::init();
+		$parentClass = get_parent_class();
+		if(isset(Yii::$app->view->themeSetting['bootstrap4']) && Yii::$app->view->themeSetting['bootstrap4'])
+			$parentClass = 'yii\bootstrap4\ActiveForm';
+
+		return  $parentClass::end($config);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function getLayout($config)
+	{
+		if(!isset($config['options']['class']))
+			return [];
+
+		if(preg_match('/(form-horizontal)/', $config['options']['class']))
+			return ['layout'=>'horizontal'];
+
+		if(preg_match('/(form-inline)/', $config['options']['class']))
+			return ['layout'=>'inline'];
 	}
 }
