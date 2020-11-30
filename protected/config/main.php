9cff39e818089f3019895401c7e9b973b4e704df
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2020-11-30 13:22:37 +0700

    fixed bug cookieValidationKey is missing

--- protected/config/main.php
+++ protected/config/main.php
@@ -23,9 +23,7 @@ $config = [
 	'components' => [
 		'request' => [
 			// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-			'cookieValidationKey' => ''
-		],
-		'request' => [
+			'cookieValidationKey' => '',
 			'csrfParam' => $bn . '_csrf',
 		],
 		'cache' => [
