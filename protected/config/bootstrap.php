eb0badc9ded3318d4ec97f3d795e570ecba3af3c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2021-05-10 17:21:07 +0700

    add config move themes to protected

--- protected/config/bootstrap.php
+++ protected/config/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 Yii::setAlias('@webroot', dirname(dirname(__DIR__)));
 Yii::setAlias('@app', dirname(dirname(__DIR__)) . '/protected');
-Yii::setAlias('@themes', dirname(dirname(__DIR__)) . '/themes');
+Yii::setAlias('@themes', dirname(dirname(__DIR__)) . '/protected/themes');
 Yii::setAlias('@public', dirname(dirname(__DIR__)) . '/public');
 Yii::setAlias('@mail', dirname(dirname(__DIR__)) . '/mail');
 Yii::setAlias('@modules', '@app/modules');
